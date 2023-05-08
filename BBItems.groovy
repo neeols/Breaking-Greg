cd7d0b1410ac44225e66a89cde2f5010462ca481
neeols <101673369+neeols@users.noreply.github.com>
2023-05-07 19:50:17 -0700

    Crystal Meth Item

---
+++ BBItems.groovy
@@ -0,0 +1,10 @@
+import net.minecraft.item.EnumRarity
+import net.minecraft.item.ItemStack
+import net.minecraft.init.MobEffects
+import net.minecraft.potion.PotionEffect
+import net.minecraft.potion.Potion
+import net.minecraft.item.Item
+
+import static Util.createItem
+
+createItem("crystal meth", EnumRarity.EPIC, 64)

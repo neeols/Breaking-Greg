a9b18939cad42302efd12707c294a23f9a9eae84
Brownbeltninja <89230590+Brownbeltninja@users.noreply.github.com>
2023-05-08 15:48:35 +1200

    full port bar syringe

--- BBRecipes.groovy
+++ BBRecipes.groovy
@@ -1,14 +1,22 @@
     mods.gregtech.large_chemical_reactor.recipeBuilder() // Chloroacetone
         .inputs(metaitem('dustAluminiumTrichloride') * 15)
-        .fluidInputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
+        .inputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
         .fluidOutputs(fluid('chloroacetone') * 20000)
         .duration(200) // 10 secondssitll 
         .EUt(2048) // EV recipe
      .buildAndRegister()
 
+    mods.gregtech.large_chemical_reactor.recipeBuilder() // phenylacetone
+        .inputs(metaitem('dustAluminiumTrichloride') * 2)
+        .inputs(liquid('benzene') * 44000, liquid('chloroacetone') * 7000)
+        .fluidOutputs(liquid('phenylacetone') * 6000)
+        .duration(200)
+        .EUt(1048)
+        .buildAndRegister();
+
     mods.gregtech.chemical_reactor.recipeBuilder() // lithium hydride
         .inputs(ore('dustLithium')
-        .fluidINputs(liquid('hydrogen') * 1000)
+        .inputs(liquid('hydrogen') * 1000)
         .outputs(metaitem('dustLithiumHydride'))
         .duration(200)
         .EUt(120)
@@ -16,9 +24,50 @@
 
     mods.gregtech.blast_furnace.recipeBuilder() //ALuminium Trichloride
             .inputs(ore('dustAluminium') * 2)
-            .fluidItems(liquid('hydrochloric_acid') * 6000)
+            .items(liquid('hydrochloric_acid') * 6000)
             .outputs(metaitem('dustAluminiumTrichloride') * 2)
             .duration(200)
             .EUt(120)
             .buildAndRegister()
 
+    mods.gregtech.chemical_reactor.recipeBuilder() //Lithium Aluminium Hydride
+            .inputs(metaitem('dustLithiumHydride') * 4)
+            .inputs(liquid('aluminium_trichloride') * 1000)
+            .outputs(metaitem('dustLithiumAluminiumHydride'), metaitem('dustLithiumChloride') * 3)
+            .duration(200)
+            .EUt(120)
+            .buildAndRegister()
+    
+    mods.gregtech.chemical_reactor.recipeBuilder() // methylamine
+            .fluidInputs(liquid('methanol') * 1000, liquid('ammonia') * 1000)
+            .fluidOutputs(liquid('water') * 1000, liquid('methylamine') * 1000)    
+            .circuitMeta(10)    
+            .duration(200)    
+            .EUt(120)    
+            .buildAndRegister()    
+                
+    mods.gregtech.chemical_reactor.recipeBuilder()
+            .inputs(metaitem('dustLithiumHydride'))
+            .inputs(liquid('methylamine') * 1000, liquid('phenylacetone') * 500)   
+            .fluidOutputs(liquid('methamphetamine') * 1000)    
+            .duration(200)    
+            .EUt(8192)    
+            .buildAndRegister()  
+              
+    mods.gregtech.fluid_solidifier.recipeBuilder()
+            .inputs(liquid('methamphetamine') * 1000)
+            .notConsumable(metaitem('shape.mold.plate'))
+            .outputs(metaitem('plateMethamphetamine'))    
+            .duration(10)    
+            .EUt(120)
+            .buildAndRegister()    
+                
+    mods.gregtech.forge_hammer.recipeBuilder()
+            .inputs(metaitem('plateMethamphetamine')
+            .outputs(item('contenttweaker:emptysyringe')    
+            .duration(10)
+            .EUt(120)           
+            .buildAndRegister()         
+                     
+                     
+                     

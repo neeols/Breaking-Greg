98482e03c8d0bd5431e5ae521788bcf0d0d5f0aa
Brownbeltninja <89230590+Brownbeltninja@users.noreply.github.com>
2023-05-08 15:10:25 +1200

    Update BBRecipes.groovy

--- BBRecipes.groovy
+++ BBRecipes.groovy
@@ -1,9 +1,24 @@
-mods.gregtech.large_chemical_reactor.recipeBuilder() // Chloroacetone
-    .inputs(metaitem('dustAluminiumTrichloride') * 15)
-    .fluidInputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
-    .fluidOutputs(fluid('chloroacetone') * 20000)
-    .duration(200) // 10 secondssitll 
-    .EUt(512) // HV recipe
-    .buildAndRegister()
+    mods.gregtech.large_chemical_reactor.recipeBuilder() // Chloroacetone
+        .inputs(metaitem('dustAluminiumTrichloride') * 15)
+        .fluidInputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
+        .fluidOutputs(fluid('chloroacetone') * 20000)
+        .duration(200) // 10 secondssitll 
+        .EUt(2048) // EV recipe
+     .buildAndRegister()
 
-// not confident this is how fluids work lol
+    mods.gregtech.chemical_reactor.recipeBuilder() // lithium hydride
+        .inputs(ore('dustLithium')
+        .fluidINputs(liquid('hydrogen') * 1000)
+        .outputs(metaitem('dustLithiumHydride'))
+        .duration(200)
+        .EUt(120)
+        .buildAndRegister()
+
+    mods.gregtech.blast_furnace.recipeBuilder() //ALuminium Trichloride
+            .inputs(ore('dustAluminium') * 2)
+            .fluidItems(liquid('hydrochloric_acid') * 6000)
+            .outputs(metaitem('dustAluminiumTrichloride') * 2)
+            .duration(200)
+            .EUt(120)
+            .buildAndRegister()
+

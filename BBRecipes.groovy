21f8f4bc54791a182f18e03198261b1b767a6946
Brownbeltninja <89230590+Brownbeltninja@users.noreply.github.com>
2023-05-08 14:46:16 +1200

    Add Chloroacetone
    no clue how this shit works need to get on my workstation to test

--- BBRecipes.groovy
+++ BBRecipes.groovy
@@ -1,1 +1,9 @@
+mods.gregtech.large_chemical_reactor.recipeBuilder() // Chloroacetone
+    .inputs(metaitem('dustAluminiumTrichloride') * 15)
+    .fluidInputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
+    .fluidOutputs(fluid('chloroacetone') * 20000)
+    .duration(200) // 10 secondssitll 
+    .EUt(512) // HV recipe
+    .buildAndRegister()
 
+// not confident this is how fluids work lol

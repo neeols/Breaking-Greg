f2ae964a701bddf26944e11eca02fd15be08bd59
neeols <101673369+neeols@users.noreply.github.com>
2023-05-10 19:02:57 -0700

    Update BBRecipes.groovy

--- postInit/BBRecipes.groovy
+++ postInit/BBRecipes.groovy
@@ -7,28 +7,28 @@ fluid_solidifier = recipemap('fluid_solidifier')
 forge_hammer = recipemap('forge_hammer')
 
     lcr.recipeBuilder() // Chloroacetone
-        .inputs(metaitem('dustAluminiumTrichloride') * 15)
-        .inputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
-        .fluidOutputs(fluid('chloroacetone') * 10000)
-        .duration(200) // 10 seconds
-        .EUt(2048) // EV recipe
-        .buildAndRegister()
+            .inputs(metaitem('dustAluminiumTrichloride') * 15)
+            .inputs(fluid('chlorine') * 10000, fluid('acetone') * 10000)
+            .fluidOutputs(fluid('chloroacetone') * 10000)
+            .duration(200) // 10 seconds
+            .EUt(2048) // EV recipe
+            .buildAndRegister()
 
    lcr.recipeBuilder() // phenylacetone
-        .inputs(metaitem('dustAluminiumTrichloride') * 2)
-        .inputs(liquid('benzene') * 44000, liquid('chloroacetone') * 7000)
-        .fluidOutputs(liquid('phenylacetone') * 6000)
-        .duration(200)
-        .EUt(1048)
-        .buildAndRegister();
+            .inputs(metaitem('dustAluminiumTrichloride') * 2)
+            .inputs(liquid('benzene') * 44000, liquid('chloroacetone') * 7000)
+            .fluidOutputs(liquid('phenylacetone') * 6000)
+            .duration(200)
+            .EUt(1048)
+            .buildAndRegister();
 
     chemical_reactor.recipeBuilder() // lithium hydride
-        .inputs(ore('dustLithium')
-        .inputs(liquid('hydrogen') * 1000)
-        .outputs(metaitem('dustLithiumHydride'))
-        .duration(200)
-        .EUt(120)
-        .buildAndRegister()
+            .inputs(ore('dustLithium')
+            .inputs(liquid('hydrogen') * 1000)
+            .outputs(metaitem('dustLithiumHydride'))
+            .duration(200)
+            .EUt(120)
+            .buildAndRegister()
 
     ebf.recipeBuilder() //ALuminium Trichloride
             .inputs(ore('dustAluminium') * 2)
@@ -45,39 +45,39 @@ forge_hammer = recipemap('forge_hammer')
             .duration(200)
             .EUt(120)
             .buildAndRegister()
-    
+
     chemical_reactor.recipeBuilder() // methylamine
             .fluidInputs(liquid('methanol') * 2000, liquid('ammonia') * 2000)
-            .fluidOutputs(liquid('water') * 1000, liquid('methylamine') * 1000)    
-            .circuitMeta(10)    
-            .duration(200)    
-            .EUt(120)    
-            .buildAndRegister()    
-                
+            .fluidOutputs(liquid('water') * 1000, liquid('methylamine') * 1000)
+            .circuitMeta(10)
+            .duration(200)
+            .EUt(120)
+            .buildAndRegister()
+
     chemical_reactor.recipeBuilder()
             .inputs(metaitem('dustLithiumHydride') * 2)
-            .inputs(liquid('methylamine') * 2000, liquid('phenylacetone') * 1000)   
+            .inputs(liquid('methylamine') * 2000, liquid('phenylacetone') * 1000)
             .fluidOutputs(liquid('methamphetamine') * 2000)
             .cleanroom(CleanroomType.CLEANROOM)
-            .duration(200)    
-            .EUt(8192)    
-            .buildAndRegister()  
-              
+            .duration(200)
+            .EUt(8192)
+            .buildAndRegister()
+
     fluid_solidifier.recipeBuilder()
             .inputs(liquid('methamphetamine') * 1000)
             .notConsumable(metaitem('shape.mold.plate'))
-            .outputs(metaitem('plateMethamphetamine'))    
-            .duration(10)    
+            .outputs(metaitem('plateMethamphetamine'))
+            .duration(10)
             .EUt(120)
-            .buildAndRegister()    
-                
+            .buildAndRegister()
+
     forge_hammer.recipeBuilder()
             .inputs(metaitem('plateMethamphetamine'))
-            .outputs(item('nomifactory:crystalmeth')) 
+            .outputs(item('nomifactory:crystalmeth'))
             .duration(10)
-            .EUt(120)           
-            .buildAndRegister()         
-                     
+            .EUt(120)
+            .buildAndRegister()
+
    crafting.shapedBuilder()
             .name('empty_syringe')
             .output(item('nomifactory:emptysyringe'))
@@ -87,4 +87,4 @@ forge_hammer = recipemap('forge_hammer')
             .key('N', item('nomifactory:needle'))
             .key('R', metaitem('rodIron'))
             .key('P', item('nomifactory:plunger'))
-            .register()        
+            .register()
